c9fa8903e9fdd48969f0ed7c6da37f4ee8da485c
Mourad ElSheraey <mourad.elsheraey@gmail.com>
2023-02-28 01:57:38 +0200

    Add scripts

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # bash-auth0-ulp
+`ulp.sh` is a bash script that allows you to manage the New Universal Login Page templates API in Auth0.
+
+## Prerequisites
+Before using ulp.sh, you must have the following:
+
+- An Auth0 account.
+- An Auth0 Management API token with the necessary permissions for template management.
+- curl installed on your system
+
+## Usage
+Simply run the script with `bash ./ulp.sh`.
+
+## Configuration
+The script supports reading the Auth0 Management API token and endpoint URLs from a config file. Please review `ulp.config` for more information.
---
+++ ulp.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Read the config file
+. ./ulp.config
+
+# Define a function to set the template
+set_template() {
+
+  # Send the API request to set the template
+  curl --request PUT \
+    --url "${TEMPLATE_ENDPOINT}" \
+    --header "authorization: Bearer ${MGMT_API_ACCESS_TOKEN}" \
+    --header 'content-type: text/html' \
+    --data-binary "@ulp.html" \
+
+  echo "Success..."
+}
+
+# Define a function to get the template
+get_template() {
+
+  # Send the API request to get the template
+  curl --request GET \
+    --url "${TEMPLATE_ENDPOINT}" \
+    --header "authorization: Bearer ${MGMT_API_ACCESS_TOKEN}" \
+    --w "\n"
+
+  echo "Success..."
+}
+
+# Define a function to delete the template
+delete_template() {
+
+  # Send the API request to delete the template
+  curl --request DELETE \
+    --url "${TEMPLATE_ENDPOINT}" \
+    --header "authorization: Bearer ${MGMT_API_ACCESS_TOKEN}" \
+
+  echo "Success..."
+}
+
+# Loop until the user chooses to exit
+while true; do
+
+  # Prompt the user to choose an action
+  echo "Choose an action:"
+  echo "1. Set the template"
+  echo "2. Get the template"
+  echo "3. Delete the template"
+  echo "0. Exit"
+  read -p "Enter your choice (0-3): " CHOICE
+
+  # Call the appropriate function based on the user's choice
+  case "${CHOICE}" in
+    1) set_template;;
+    2) get_template;;
+    3) delete_template;;
+    0) echo "Exiting..."; exit;;
+    *) echo "Invalid choice";;
+  esac
+done
